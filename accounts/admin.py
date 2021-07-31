9915a14929846ece417a064bfab7aa00ef1860b3
marufyuldashev <cool.backstreet@gmail.com>
2021-07-31 15:58:11 +0500

    add model and form

---
+++ accounts/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ['username', 'email', 'first_name', 'last_name', 'is_staff']
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('address',)}),
+    )
+    add_fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('address',)}),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'blogapp',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +126,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
