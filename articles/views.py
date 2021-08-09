4a35a074055e261ce0ecac30b502681121ade440
marufyuldashev <cool.backstreet@gmail.com>
2021-08-09 09:58:51 +0500

    add permission for users

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from django.urls import reverse_lazy
@@ -14,20 +15,42 @@ class ArticleDetailView(DetailView):
     template_name = 'article_detail.html'
 
 
-class ArticleUpdateView(UpdateView):
+class ArticleUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Article
     fields = ('title', 'summary', 'body', 'photo')
     template_name = 'article_edit.html'
 
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
 
-class ArticleDeleteView(DeleteView):
+
+# UserPassesTestMixin boshqa userni maqolasini uzgartirishga yo'l qo'ymaydi
+class ArticleDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Article
     template_name = 'article_delete.html'
     success_url = reverse_lazy('article_list')
 
+    # author bilan user bir xilligini tekshiradi
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
 
-class ArticleCreateView(CreateView):
+
+# LoginRequiredMixin ArticleCreateView sahifasi ochilishidan oldin user saytdan
+# login qilib kirganmi yumi tekshiradi
+class ArticleCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = Article
     template_name = 'article_new.html'
-    fields = ('title', 'summary', 'body', 'photo', 'author',)
+    fields = ('title', 'summary', 'body', 'photo',)
+
+    def form_valid(self, form):
+        """Aynan qaysi user avtorizatsiya bo'lgan bo'lsa author avtomatik o'zi bo'ladi
+        boshqa userni nomi chiqishiga yo'l qo'ymaydi"""
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    # user superuser ekanini tekshirish
+    def test_func(self):
+        return self.request.user.is_superuser
 
